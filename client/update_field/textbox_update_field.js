dff6e61887d795bae7ae854db31aabc198e9c8b9
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-04-05 02:37:48 +0700

    git textbox duplicate id body

--- client/update_field/textbox_update_field.js
+++ client/update_field/textbox_update_field.js
@@ -1,29 +1,31 @@
 Template.textbox_list_update_field.onRendered(function () {
-    var that= this;
+    var that = this;
+    var nameField = that.data.name;
     this.autorun(function () {
-        var dom = document.getElementById("tab-content-textbox-list");
+        var dom = document.getElementById("tab-content-textbox-list-" + nameField);
         $(dom).html('');
-        var key =  Session.get('current_unique_key');
+        var key = Session.get('current_unique_key');
         if (!key)
             return;
         Blaze.renderWithData(Template.tab_content_textbox_list, {
-            data: ENUM.getValue(Router.current().params.collection_name,that.data.name,key),
-            name: that.data.name
+            data: ENUM.getValue(Router.current().params.collection_name, nameField, key),
+            name: nameField
         }, dom);
         $('textarea').froalaEditor();
     });
 })
-Template.textbox_update_field.onRendered(function(){
-    var that= this;
+Template.textbox_update_field.onRendered(function () {
+    var that = this;
+    var nameField = that.data.name;
     this.autorun(function () {
-        var dom = document.getElementById("textbox-panel-body");
+        var dom = document.getElementById("textbox-panel-body-" + nameField);
         $(dom).html('');
-        var key =  Session.get('current_unique_key');
+        var key = Session.get('current_unique_key');
         if (!key)
             return;
         Blaze.renderWithData(Template.textbox_panel_body, {
-            data: ENUM.getValue(Router.current().params.collection_name,that.data.name,key),
-            name: that.data.name
+            data: ENUM.getValue(Router.current().params.collection_name, nameField, key),
+            name: nameField
         }, dom);
         $('textarea').froalaEditor();
     });
